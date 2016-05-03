2fbafe2b4e9f827b6c7674ee51917c0d1e3acfc3
woinck <woinck@gmail.com>
2016-05-04 01:06:41 +0800

    Add http API
    Signed-off-by: woinck <woinck@gmail.com>

--- nics_lpirc/api/http.py
+++ nics_lpirc/api/http.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import os
 import numpy as np
+import cv2
 
 from nics_lpirc.api import client
 from nics_lpirc.api import APIAdapter
 
 class HttpAPI(APIAdapter):
-    def __init__(self, usrname, passwd):
-        [self.token, self.status] = client.get_token(usrname, passwd)
+    def __init__(self, cfg):
+        self.cfg = cfg.HttpAPI
+        client.set_cfgs(self.cfg)
+        [self.token, self.status] = client.get_token(self.cfg.username, self.cfg.password)
         [self.total_num, status] = client.get_no_of_images(self.token)
         self.get_idx = 0
         self.post_idx = 0
-        print "Total num:", self.total_num
+        # print "Total num:", self.total_num
         self.total_num = int(self.total_num)
 
     def get_image(self):
@@ -20,13 +24,15 @@ class HttpAPI(APIAdapter):
         status = 0
         try:
             status = client.get_image(self.token, self.get_idx + 1)
-            res = '../images/%d.jpg'%(self.get_idx + 1)
+            res = os.path.join(self.cfg.image_directory,'%d.jpg'%(self.get_idx + 1))
+            im = cv2.imread(res)
+            # res = '../images/%d.jpg'%(self.get_idx + 1)
         except Exception as e:
             print "Exception:", e
             res = ''
         self.get_idx += 1
         print "Image download. Current idx:", self.get_idx
-        return (self.get_idx, res)
+        return str(self.get_idx)+'.jpg', im
     
     def get_images(self):
         if self.done():
@@ -34,7 +40,8 @@ class HttpAPI(APIAdapter):
         status = 0
         try:
             status = client.get_image(self.token, self.get_idx + 1)
-            res = '../images/%d.zip'%(self.get_idx + 1)
+            res = os.path.join(self.cfg.image_directory,'%d.zip'%(self.get_idx + 1))
+            # res = '../images/%d.zip'%(self.get_idx + 1)
         except Exception as e:
             print "Exception:", e
             res = ''
@@ -43,7 +50,10 @@ class HttpAPI(APIAdapter):
         return (self.get_idx, res)
 
 
-    def commit_result(self,image_id, class_ids, confidences, bboxs):
+    def commit_result(self,image_id, class_ids, dets):
+        print dets
+        confidences = [x[0] for x in dets]
+        bboxs = [x[1:5] for x in dets]
         if len(class_ids) != len(confidences) and len(class_ids) != len(bboxs):
             print "Index does not match!"
             return 0
