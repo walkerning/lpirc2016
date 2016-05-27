ce363cecb2a3bb1d434094f5c624033d02bb8d4c
walkerning <foxdoraame@gmail.com>
2016-05-27 16:46:08 +0800

    Fix a bug: use the same gpu in gpu_nms

--- nics_lpirc/detector.py
+++ nics_lpirc/detector.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from fast_rcnn.test import im_detect
+from fast_rcnn.config import cfg as f_cfg
 import caffe
 
 class Detector(object):
@@ -13,6 +14,7 @@ class Detector(object):
         else:
             caffe.set_mode_gpu()
             caffe.set_device(cfg.detector.device_id)
+            f_cfg.GPU_ID = cfg.detector.device_id
 
     def detect(self, im):
         scores, boxes = im_detect(self.net, im)
